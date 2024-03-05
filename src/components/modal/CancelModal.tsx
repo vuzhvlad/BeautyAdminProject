97a4642296cda786918fd19850834741d9f33c94
vuzhvlad <vuzh.vlad@gmail.com>
2024-03-05 16:57:38 +0100

    created a portal for our modal window that puts modal window over other elements, used useLayoutEffect that renders before the browser, worked with a basic js inside of portal

--- src/components/modal/CancelModal.tsx
+++ src/components/modal/CancelModal.tsx
@@ -1,3 +1,4 @@
+import Portal from "../portal/portal";
 import "./modal.scss";
 
 interface IModalProps {
@@ -7,20 +8,22 @@ interface IModalProps {
 
 function CancelModal({ handleClose, selectedId }: IModalProps) {
   return (
-    <div className="modal">
-      <div className="modal__body">
-        <span className="modal__title">
-          Are you sure you want to delete the appointment? {selectedId}
-        </span>
-        <div className="modal__btns">
-          <button className="modal__ok">Ok</button>
-          <button className="modal__close" onClick={() => handleClose(false)}>
-            Close
-          </button>
+    <Portal>
+      <div className="modal">
+        <div className="modal__body">
+          <span className="modal__title">
+            Are you sure you want to delete the appointment? {selectedId}
+          </span>
+          <div className="modal__btns">
+            <button className="modal__ok">Ok</button>
+            <button className="modal__close" onClick={() => handleClose(false)}>
+              Close
+            </button>
+          </div>
+          <div className="modal__status">Success</div>
         </div>
-        <div className="modal__status">Success</div>
       </div>
-    </div>
+    </Portal>
   );
 }
 
---
+++ src/components/portal/portal.tsx
@@ -0,0 +1,47 @@
+import { useState, useLayoutEffect, ReactNode } from "react";
+import { createPortal } from "react-dom";
+
+interface PortalProps {
+  children: ReactNode;
+  wrapperId?: string;
+}
+
+function createWrapperAndAppendToBody(wrapperId: string) {
+  // function that creates a wrapper for our element
+  const wrapperElement = document.createElement("div");
+  wrapperElement.setAttribute("id", wrapperId);
+  wrapperElement.setAttribute("class", "modal");
+  document.body.append(wrapperElement);
+
+  return wrapperElement;
+}
+
+function Portal({ children, wrapperId = "portal-wrapper" }: PortalProps) {
+  const [wrapperElement, setwrapperElement] = useState<HTMLElement | null>(
+    null
+  );
+
+  useLayoutEffect(() => {
+    // hook for creating layout
+    let element = document.getElementById(wrapperId);
+    let created = false;
+
+    if (!element) {
+      created = true; // flag if element was created and we didnt find it before
+      element = createWrapperAndAppendToBody(wrapperId);
+    }
+
+    setwrapperElement(element);
+
+    return () => {
+      // removing element every time if it was created by us
+      if (created) {
+        element?.remove();
+      }
+    };
+  }, [wrapperId]);
+
+  if (wrapperElement === null) return null;
+
+  return createPortal(children, wrapperElement);
+}
